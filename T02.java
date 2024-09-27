33ef99230aaa1c090c855aae41a46437fc411576
DavinaHutabarat <dggxqh@gmail.com>
2024-09-27 14:27:12 +0700

    Selesai

--- T02.java
+++ T02.java
@@ -1,1 +1,43 @@
-// NIM - Your Name
+// 12S24047 - Davina Olivia Yosefanny Hutabarat
+// 12S24050 - Ester Hasianna Nainggolan
+import java.util.*;
+import java.lang.Math;
+
+public class T02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN, judul, penulis, penerbit, formatBukuElektronik, kategori;
+        int tahunTerbit, stok;
+        double hargaPembelian, minimumMargin, rating;
+
+        iSBN = input.nextLine();
+        judul = input.nextLine();
+        penulis = input.nextLine();
+        tahunTerbit = Integer.parseInt(input.nextLine());
+        penerbit = input.nextLine();
+        formatBukuElektronik = input.nextLine();
+        hargaPembelian = Double.parseDouble(input.nextLine());
+        minimumMargin = Double.parseDouble(input.nextLine());
+        stok = Integer.parseInt(input.nextLine());
+        rating = Double.parseDouble(input.nextLine());
+        if (rating >= 4.7) {
+            kategori = "Best Pick";
+        } else {
+            if (rating >= 4.5) {
+                kategori = "Must Read";
+            } else {
+                if (rating >= 4.0) {
+                    kategori = "Recommended";
+                } else {
+                    if (rating >= 3.0) {
+                        kategori = "Average";
+                    } else {
+                        kategori = "Low";
+                    }
+                }
+            }
+        }
+        System.out.println(iSBN + "|" + judul + "|" + penulis + "|" + tahunTerbit + "|" + penerbit + "|" + formatBukuElektronik + "|" + hargaPembelian + "|" + minimumMargin + "|" + stok + "|" + rating + "|" + kategori);
+    }
+}
